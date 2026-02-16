84cf2b6985cd9c8d7466c405435004fab66e3f12
Bhumik14 <bhumikrane05@gmail.com>
2026-02-16 13:34:10 +0530

    updated: quiz generation:

--- backend/src/controllers/quiz.controller.js
+++ backend/src/controllers/quiz.controller.js
@@ -12,14 +12,14 @@ export async function quizCreationController(req, res) {
     const body = {
       topic: req.body.topic,
       purpose: req.body.purpose || "Learning",
-      previous_q_summary: req.body.previous_q_summary || [],
+      num_questions: req.body.num_questions || [],
     };
 
     const response = await fastapiClient.post("/roadmap/create-quiz", body);
 
     return res.status(201).json({
       message: "Quiz question created successfully",
-      question: response.data,
+      quiz: response.data,
     });
   } catch (err) {
     console.error("Error in Quiz Creation:", err);
